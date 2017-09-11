b0ef54d6d9c838286ff696fc960b95de8b3f7cf8
Alexandra-Hernandez <30915658+Alexandra-Hernandez@users.noreply.github.com>
2017-09-11 11:37:48 -0500

    Add files via upload

---
+++ quicksort.py
@@ -0,0 +1,20 @@
+cnt=0
+
+def quicksort(arr):
+	global cnt
+	if len(arr)<=1:
+		return arr
+	p=arr.pop(0)
+	menores,mayores=[],[]	
+	for e in arr:
+		cnt+=1
+		if e<=p:
+			menores.append(e)	
+		else:
+			mayores.append(e)
+	return quicksort(menores) + [p] + quicksort(mayores)
+
+a=[1, 0, 15, 6, 7]
+print(a)
+quicksort(a)
+print(cnt)
