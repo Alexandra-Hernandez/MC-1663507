7e8460ccad5047b40a8679708ca080da630c256a
Alexandra-Hernandez <30915658+Alexandra-Hernandez@users.noreply.github.com>
2017-10-08 21:58:56 -0500

    Add files via upload

--- primo.py
+++ primo.py
@@ -1,14 +1,6 @@
-def primo(n):
-	a=0
-	cnt=0
-	for i in range(1,n+1):
-		cnt=cnt+1
-		if((n%i)==0):
-			a=a+1
-	if a>2:
-		return ("Es numero compuesto"), cnt
-	if a==2:
-		return("Es numero primo"), cnt
-
-	
-		
+import math
+def primo (n):
+	for i in range(2, round(n**(1/2)+1)):
+		if n&i==0:
+			return("no es primo")
+	return ("es primo")
